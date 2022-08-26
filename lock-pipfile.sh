84eaa9556d4329c9412d853f175205e35043fe81
Yann Rosema <yann.rosema@hotmail.com>
2022-08-26 17:27:54 +0200

    typo in lock-pipfile.sh

--- lock-pipfile.sh
+++ lock-pipfile.sh
@@ -9,6 +9,6 @@ if git status -s | grep Pipfile.lock ; then
     git add Pipfile.lock
     git commit -m "Lock Pipfile"
 fi
-if ["$my_bool" = true]; then
+if ["$pop" = true]; then
     git stash pop
 fi
